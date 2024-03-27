28fa021b6f3cadb024f690a444838ba176c01de3
Mlocik97 <Mlocik97@gmail.com>
2024-03-27 17:14:09 +0100

    prepare page for results

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,14 @@
-<script setup></script>
+<script setup>
 
-<template></template>
+</script>
+
+<template>
+	<div>
+		<form action="/search" method="post">
+			<label for="prompt">
+				<input type="text" id="prompt">
+			</label>
+			<button type="submit">Search</button>
+		</form>
+	</div>
+</template>
---
+++ pages/results.vue
@@ -0,0 +1,24 @@
+<script setup>
+	/**
+	 * @type {{
+	 * 		text: string,
+	 * 		link: string
+	 * }[]}
+	 */
+	const results = [];
+</script>
+
+<template>
+	<div>
+		<ul v-for="result in results">
+			<li>
+				<span>{{ result.text }}</span>
+				<a :href="result.link">Details</a>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<style>
+	@import url('./styles/results.css');
+</style>
