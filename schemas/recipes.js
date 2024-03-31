10387d4a26e1bb70549d4c1f08023baed86d5efb
Mlocik97 <Mlocik97@gmail.com>
2024-03-31 16:17:56 +0200

    add schema

--- schemas/recipes.js
+++ schemas/recipes.js
@@ -1,7 +1,13 @@
 import { Schema, model } from 'mongoose';
 
 export const recipes_schema = new Schema({
-	
+	recipes: {
+		type: [{
+			name: String,
+			text: String,
+			category: String
+		}],
+	}
 })
 
 export const recipes_model = model('recipes', recipes_schema, 'recipes');
