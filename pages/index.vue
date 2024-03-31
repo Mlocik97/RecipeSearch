80cf04712509043904eb0aec0757ce98c376baf6
Mlocik97 <Mlocik97@gmail.com>
2024-03-31 17:02:30 +0200

    implement search page

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,23 @@
 <script setup>
+	const prompt = ref('');
+	const router = useRouter();
 
+	async function executeSearch() {
+		const data = await $fetch('/api/recipes').then((r) => r.json());
+		router.push({ // after we fetch data, we redirect to page that shows these data
+			path: '/results',
+			query: { data: JSON.stringify(data) }
+		})
+	}
 </script>
 
 <template>
 	<div>
-		<form action="/search" method="post">
+		<form action="/api/recipes" method="post">
 			<label for="prompt">
-				<input type="text" id="prompt">
+				<input type="text" id="prompt" v-bind="prompt">
 			</label>
-			<button type="submit">Search</button>
+			<button type="submit" @click="executeSearch">Search</button>
 		</form>
 	</div>
 </template>
