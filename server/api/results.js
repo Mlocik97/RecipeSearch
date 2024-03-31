9a4b828fdc3db22f258ec5f58794963b794f3ea5
Mlocik97 <Mlocik97@gmail.com>
2024-03-31 16:48:46 +0200

    implement dumy API.

--- server/api/results.js
+++ server/api/results.js
@@ -1,6 +1,15 @@
-export default defineEventHandler(async (event) => {
+export default defineEventHandler(async ({ data }) => {
+
+	/** @TODO add external API */
+	const recipes = await fetch('~TODO~', {
+		method: "POST",
+		body: JSON.stringify({
+			query: data.search_prompt // or something like that. Will see when I will find external API.
+		})
+	}).then((r) => r.json());
+
 	/** @TODO add recipes, you fetch from external API */
 	return {
-		recipes: []
+		recipes: recipes || []
 	}
 })
